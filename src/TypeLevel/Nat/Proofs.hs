3097e8856156548ea3a081b420c198d20cd9f26f
Donnacha Oisín Kidney <mail@doisinkidney.com>
2017-04-21 17:38:06 +0100

    better naming for nat proof

--- src/TypeLevel/Nat/Proofs.hs
+++ src/TypeLevel/Nat/Proofs.hs
@@ -2,6 +2,8 @@
 {-# LANGUAGE TypeFamilies  #-}
 {-# LANGUAGE DataKinds     #-}
 
+{-# OPTIONS_GHC -fno-warn-unticked-promoted-constructors #-}
+
 -- | Some proofs on type-level Peano numbers.
 module TypeLevel.Nat.Proofs where
 
@@ -17,13 +19,13 @@ plusAssoc (Sy x) y z = case plusAssoc x y z of
 {-# NOINLINE plusAssoc #-}
 
 -- | Zero is the identity of addition.
-addZeroZero :: The Nat x -> x + 'Z :~: x
-addZeroZero Zy = Refl
-addZeroZero (Sy x) = case addZeroZero x of
+plusZeroNeutral :: The Nat n -> n + Z :~: n
+plusZeroNeutral Zy = Refl
+plusZeroNeutral (Sy n) = case plusZeroNeutral n of
   Refl -> Refl
-{-# NOINLINE addZeroZero #-}
+{-# NOINLINE plusZeroNeutral #-}
 
 {-# RULES
 "plusAssoc" forall x y z. plusAssoc x y z = unsafeCoerce (Refl :: 'Z :~: 'Z)
-"addZeroZero" forall x. addZeroZero x = unsafeCoerce (Refl :: 'Z :~: 'Z)
+"plusZeroNeutral" forall x. plusZeroNeutral x = unsafeCoerce (Refl :: 'Z :~: 'Z)
  #-}
